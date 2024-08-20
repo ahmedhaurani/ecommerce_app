14b763cf5fbf9d1ca0fb0d2fa811450792b1d297
ahmedhaurani <ahmed.haurani@gmail.com>
2024-08-20 12:08:46 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     'node_modules/preline/dist/*.js',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+// //   plugins: [
+// //     require('preline/plugin'),
+
+// //   ],
+// // }
+
+// import typography from '@tailwindcss/typography';
+// import forms from '@tailwindcss/forms';
+// import aspectRatio from '@tailwindcss/aspect-ratio';
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./resources/**/*.blade.php",
+//     "./resources/**/*.js",
+//     'node_modules/preline/dist/*.js',
+
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     typography,
+//     forms,
+//     aspectRatio,
+//     require('preline/plugin'),
+
+//   ],
+// }
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
